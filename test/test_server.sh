7cf4069730886a1ad5e72ca19995019c8145486c
brownsend <mfzhu@sending.tech>
2025-11-11 15:13:36 +0800

    合并封面页和初始化config配置

--- test/test_server.sh
+++ test/test_server.sh
@@ -4,7 +4,7 @@ curl -X POST "http://127.0.0.1:8100/api/report/basic-info" \
         "project_name": "智慧数据中心巡检项目",
         "room_name": "A区主机房",
         "year": 2025,
-        "quarter": "Q4",
+        "quarter": "4季度",
         "report_date": "2025-10-20",
         "report_person": "张三"
       }'
